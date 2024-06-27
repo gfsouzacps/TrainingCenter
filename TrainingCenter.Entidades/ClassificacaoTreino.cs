f15b8733ae0756ae3df58d3e90a4e9cf3e945304
bmoreiradev <brunomoreiraa2002@gmail.com>
2024-06-26 21:49:31 -0300

    Commit das primeiras entidades do projeto.

---
+++ TrainingCenter.Entidades/ClassificacaoTreino.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingCenter.Entidades
+{
+    public class ClassificacaoTreino
+    {
+        public int IdClassificao { get; set; }
+        public string NomeClassificacao { get; set; }
+    }
+}
---
+++ TrainingCenter.Entidades/MembrosCorpo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingCenter.Entidades
+{
+    public class MembrosCorpo
+    {
+        public int IdMembro { get; set; }
+        public string NomeMembro { get; set; }
+    }
+}
